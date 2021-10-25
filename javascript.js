f69c479c826d5320447c815904e09e834b26608e
PrathyushaAviligonda1998 <81526657+PrathyushaAviligonda1998@users.noreply.github.com>
2021-10-25 14:01:50 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').remove('sticky');
+
+        }
+    }, {
+        offset: '60px;'
+    });
+    $('.js--scroll-to-plans').click(function () {
+        $('html,body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+
+    });
+
+
+
+
+
+
+
+});
+alert("hellooo");
